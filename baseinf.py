df07b6a0e5f1c89b45ca6d3bce3855b56e4f609f
rjshk013 <rjshk013@gmail.com>
2020-11-19 11:48:06 +0530

    Add files via upload

---
+++ baseinf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import platform
+
+print('Uname:', platform.uname())
+
+print()
+print('Distribution :', platform.linux_distribution())
+print('Machine :', platform.machine())
+print('Node :', platform.node())
+print('Processor :', platform.processor())
+print('Release :', platform.release())
+print('System :', platform.system())
+print('Version :', platform.version())
+print('Platform :', platform.platform())
+
